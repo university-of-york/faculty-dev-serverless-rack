c4ef518635fd48ac51534cd11c3aa3ed2fdb34d8
mjp513 <matthew.pickin@york.ac.uk>
2026-03-04 13:33:38 +0000

    Update first spec test for rack v3 compatability
    Rack v2 -> v3 includes the following changes:
    - Require response header keys to be exclusively lower-case
    - Array objects for header values are supported
    
    Thus, ensure all response header keys are lower-case, and
    remove repeated header keys (i.e. `set-cookie`) in favour
    of an array object with multiple values

--- spec/rack_adapter_spec.rb
+++ spec/rack_adapter_spec.rb
@@ -88,11 +88,9 @@ RSpec.describe 'Rack adapter' do
     expect(@response).to eq(
       'body' => 'Hello World ☃!',
       'headers' => {
-        'set-cookie' => 'CUSTOMER=WILE_E_COYOTE',
-        'Content-Length' => '16',
-        'Content-Type' => 'text/plain',
-        'sEt-cookie' => 'LOT_NUMBER=42',
-        'Set-cookie' => 'PART_NUMBER=ROCKET_LAUNCHER_0002'
+        'set-cookie' => ['CUSTOMER=WILE_E_COYOTE', 'LOT_NUMBER=42', 'PART_NUMBER=ROCKET_LAUNCHER_0002'],
+        'content-length' => '16',
+        'content-type' => 'text/plain'
       },
       'statusCode' => 200,
       'isBase64Encoded' => false
